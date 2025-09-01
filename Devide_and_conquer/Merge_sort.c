9c2f134400bbe2b57ebdfad5bb77c4527be9bb1a
Temmasecret <temmaunan1234@gmail.com>
2025-09-01 21:53:22 +0700

    Quicksort

--- Devide_and_conquer/Merge_sort.c
+++ Devide_and_conquer/Merge_sort.c
@@ -8,12 +8,58 @@ void Merge_arr(int arr[],int l,int mid,int r)
     int N2 = r - mid;
     int *A1 = (int*)malloc(sizeof(int) * N1);
     int *A2 = (int*)malloc(sizeof(int) * N2);
+    // Insert the value into new array
+    for (int i = 0;i<N1;i++)
+    {
+        A1[i] = arr[i+l];
+    }
+    for (int i = 0;i<N2;i++)
+    {
+        A2[i] = arr[i+mid+1];
+    }
+    //Merge the array
+    int i = 0;
+    int j = 0;
+    int Num_run = l;
+    while (i<N1 && j <N2)
+    {
+        if (A1[i] < A2[j]) 
+        {
+            arr[Num_run] = A1[i];
+            i++;
+        }
+        else 
+        {
+            arr[Num_run] = A2[j];
+            j++;
+        }
+        Num_run ++;
+    }
+    while (i<N1)
+    {
+        arr[Num_run] = A1[i];
+        i++;
+        Num_run++;
+    }
+    while (i<N2)
+    {
+        arr[Num_run] = A2[j];
+        j++;
+        Num_run++;
+    }
+    free(A1);
+    free(A2);
 }
 
 void Merge_sort(int arr[],int l,int r)
 {
     if (l>= r) return;
     int mid = (r-l)/2 + l;
+    for (int i = 0;i<13;i++)
+    {
+        printf("%d ",arr[i]);
+    }
+    printf("\n");
     Merge_sort(arr,l,mid);
     Merge_sort(arr,mid+1,r);
     Merge_arr(arr,l,mid,r);
@@ -21,5 +67,11 @@ void Merge_sort(int arr[],int l,int r)
 
 int main()
 {
+    int arr[] = {23,43,123,56,564,29,126,65,20,45,21,78,46};
+    Merge_sort(arr,0,12);
+    for (int i = 0;i<13;i++)
+    {
+        printf("%d ",arr[i]);
+    }
     return 0;
 }
---
+++ Devide_and_conquer/Quick_sort.c
@@ -0,0 +1,40 @@
+#include <inttypes.h>
+#include<stdio.h>
+#include<stdlib.h>
+
+void Swap(int *a, int *b)
+{
+    int tg = *a;
+    *a = *b;
+    *b = tg;
+}
+
+void Quick_sort(int arr[],int l,int r)
+{
+    if (l>=r) return;
+    printf("%d %d \n",l,r);
+    int temp = arr[l];
+    int i = l+1;
+    int j = r;
+    while (i<j)
+    {
+        while (j > l && arr[j] >= temp) j--;
+        while (i < r && arr[i] <= temp) i++;
+        if (i < j) Swap(&arr[i],&arr[j]);
+    }
+    Swap(&arr[l],&arr[j]);
+    Quick_sort(arr,l,j-1);
+    Quick_sort(arr,j+1,r);
+}
+
+
+int main()
+{
+    int arr[] = {23,28,47,68,29,68,53,41,24,89};
+    Quick_sort(arr,0,9);
+    for (int i = 0; i<10;i++)
+    {
+        printf("%d ",arr[i]);
+    }
+    return 0;
+}
