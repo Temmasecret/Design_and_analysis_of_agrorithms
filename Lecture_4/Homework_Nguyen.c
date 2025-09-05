8db615d87be256166084b0f35768b4a093b30aa3
Temmasecret <temmaunan1234@gmail.com>
2025-09-05 09:51:09 +0700

    Full for lecture4

--- Lecture_4/Homework_Nguyen.c
+++ Lecture_4/Homework_Nguyen.c
@@ -35,15 +35,66 @@ int Binary_search(int arr[],int l,int r,int key)
 
 }
 
+void Merge_arr(int arr[],int l, int r, int mid)
+{
+    int N1 = mid - l + 1;
+    int N2 = r - mid;
+    int* L1 = (int*)malloc(sizeof(int)* N1);
+    int* L2 = (int*)malloc(sizeof(int )*N2);
+    for (int i = 0;i<N1;i++)
+    {
+        L1[i] = arr[l+i];
+    }
+    for (int i = 0;i<N2;i++)
+    {
+        L2[i] = arr[mid + i + 1];
+    }
+    int idx1 = 0;
+    int idx2 = 0;
+    int arr_count = l;
+    while (idx1 < N1 && idx2 < N2)
+    {
+        if (L1[idx1] > L2[idx2]) 
+        {
+            arr[arr_count] = L2[idx2];
+            idx2++;
+        }
+        else 
+        {
+            arr[arr_count] = L1[idx1];
+            idx1++;
+        }
+        arr_count++;
+    } 
+    while(idx1 < N1 )
+    {
+        arr[arr_count] = L1[idx1];
+        idx1 ++;
+        arr_count ++;
+    }
+    while(idx2 < N2)
+    {
+        arr[arr_count] = L2[idx2];
+        idx2++;
+        arr_count++;
+    }
+    free(L1);
+    free(L2);
+}
+
 void Merge_sort(int arr[],int l,int r)
 {
-    
+    if (l>= r || l< 0 || r < 0)  return;
+    int mid = (r-l)/2 + l;
+    Merge_sort(arr,l,mid);
+    Merge_sort(arr,mid + 1,r);
+    Merge_arr(arr,l,r,mid);
 }
 
 int main()
 {
     int arr[] = {3,5,8,9,4,2,7,5,3,9,8};
-    Quick_sort(arr,0,10);
+    Merge_sort(arr,0,10);
     for (int i = 0;i<11;i++)
     {
         printf("%d ",arr[i]);
