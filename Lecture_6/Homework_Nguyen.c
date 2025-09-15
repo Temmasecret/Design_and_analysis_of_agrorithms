84274fd706417ae2876c9d1cbf5f61a41d5882ca
Temmasecret <temmaunan1234@gmail.com>
2025-09-15 22:58:16 +0700

    Update for full

--- Lecture_6/Homework_Nguyen.c
+++ Lecture_6/Homework_Nguyen.c
@@ -56,22 +56,153 @@ void Heap_sort(int arr[],int l,int r)
     }
 }
 
+typedef struct Node Node;
+struct Node
+{
+    int pos;
+    float cost;
+    Node *next;
+};
+
+Node* MakeNode(int pos)
+{
+    Node* temp = (Node*)malloc(sizeof(Node));
+    temp->pos = pos;
+    temp->cost = 10000000000;
+    temp->next = NULL;
+    return temp;
+}
+
+void Makeline(float cost, Node** begin, Node** end)
+{
+    if (begin == NULL || end == NULL)
+    {
+        return;
+    }
+    (*begin)->cost = cost;
+    (*begin)->next = (*end);
+}
+
+void InsertLast(int newpos, float cost, Node* temp)
+{
+    Node* newbie = MakeNode(newpos);
+    Node* run = temp;
+    while (run->next !=NULL)
+    {
+        run = run->next;
+    }
+    Makeline(cost,&run,&newbie);
+}
+
+typedef struct Graph{
+    int size;
+    Node** lists;
+}Graph;
+
+Graph* InitGraph(int size)
+{
+    Graph* Newbie = (Graph*)malloc(sizeof(Graph));
+    Newbie->size = size;
+    Newbie->lists = (Node**)malloc(sizeof(Node*)*size);
+    for (int i = 0;i<size;i++)
+    {
+        Newbie->lists[i] = MakeNode(i);
+    }
+    return Newbie;
+}
+
+void Push_Graph(int oldpos,int newpos,Graph** Gr,float cost)
+{
+    Node* temp = (*Gr)->lists[oldpos];
+    InsertLast(newpos,cost,temp);
+}
 
 int main()
 {
-    int cost_m[4] = {100,50,20,10};
-    int* atm_Nhan = atm_recv(410,cost_m,4);
-    for (int i = 0;i<4;i++)
+    Graph* Gr = InitGraph(6);
+    int con = 1;
+    while(con)
     {
-        printf("%d ",atm_Nhan[i]);
+        int oldpos, newpos;
+        float cost;
+        printf("Please sign the old pos, new pos and cost : ");
+        scanf("%d %d %f", &oldpos, &newpos, &cost);
+        printf("Do u want to continue? (1 for yes, 0 for no): ");
+        scanf("%d", &con);
     }
-    printf("\n");
-    int arr[] = {53,57,15,84,23};
-    Heap_sort(arr,0,4);
-    for (int i = 0;i<5;i++)
+    float res_cost[6];
+    for (int i = 0;i<6;i++)
     {
-        printf("%d ",arr[i]);
+        res_cost[i] = 10000000000;
     }
-    printf("\n");
-    return 0;
+    Node** res_way = (Node**)malloc(sizeof(Node*)*6);
+    for (int i = 0;i<6;i++)
+    {
+        res_way[i] = MakeNode(i);
+    }
+    int idx =1;
+    res_way[0] = Gr->lists[0];
+    res_cost[0] = 0;
+    int visited[6];
+    int curr = 0;
+    for (int i = 0;i<6;i++)
+    {
+        visited[i] = 0;
+    }
+    visited[curr] = 1;
+    while (idx < 6)
+    {
+        int min_pos = -1;
+        float min_cost = 10000000000;
+        Node* temp = Gr->lists[curr];
+        while (temp->next != NULL)
+        {
+            if (visited[temp->pos] == 0 && temp->cost + res_cost[curr] < min_cost)
+            {
+                min_pos = temp->pos;
+                min_cost = temp->cost + res_cost[curr];
+            }
+            temp = temp->next;
+        }   
+        InsertLast(min_pos, min_cost, res_way[curr]);
+        visited[min_pos] = 1;
+        res_cost[min_pos] = min_cost;
+        idx++;
+        curr = min_pos;
+        if (curr == -1)
+        {
+            break;
+        }
+    }
+    for (int i = 0;i<6;i++)
+    {
+        printf("Cost from 0 to %d is %.2f\n", i, res_cost[i]);
+        Node* temp = res_way[i];
+        printf("Path to %d: ", i);
+        while (temp != NULL)
+        {
+            printf("%d ", temp->pos);
+            temp = temp->next;
+        }
+        printf("\n");
+    }
 }
+/*
+    20 0 1
+    15 0 2
+    80 0 4
+    40 1 0
+    10 1 4
+    30 1 5
+    20 2 0
+    4 2 1
+    10 2 5
+    36 3 0
+    18 3 1
+    15 3 2
+    90 4 2
+    15 4 3
+    45 5 2
+    4 5 3
+    10 5 4
+*/
